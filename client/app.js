e806668005d7e239e35e4a9067612b1278ab584c
Don Khan <HCDevid@gmail.com>
2016-01-17 01:45:33 -0500

    Added anti:fake and dburles: factory packages, pulling messages from Mongo into chat feed is giving an error

---
+++ client/app.js
@@ -0,0 +1,14 @@
+Template.messages.helpers({
+	messages: Messages.find();
+})
+
+Template.footer.events({
+	'keypress input': function(event) {
+		if (event.charcode == 13) {
+			event.stopPropagation();
+			$('.message-history').append('<div class = "message"><a href="" class = "message_profile_pic"></a><a href="" class="message_username">scotch</a><span class="message_timestamp">1:31 AM</span><span class="message_star"></span><span class="message_content">' + $('.input-box_text').val() + '</span></div>');
+			$('.input-box_text').val("");
+			return false;
+		}
+	}
+})
