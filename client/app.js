01330f2854248332dbfb21ba5ea1b42a299b6417
Don Khan <HCDevid@gmail.com>
2016-01-19 20:12:37 -0500

    Added timestamp and dynamic user displays, modified factory to handle new faked parameters

--- client/app.js
+++ client/app.js
@@ -2,6 +2,25 @@ Accounts.ui.config({
 	passwordSignupFields: "USERNAME_AND_EMAIL"
 });
 
+Template.registerHelper("usernameFromId", function (userId) {
+	var user = Meteor.users.findOne({_id: userId});
+	if (typeof user == 'undefined') {
+		return 'Anonymous';
+	}
+	if (typeof user.services.github !== 'undefined') {
+		return user.services.github.username;
+	}
+	return user.username;
+});
+
+Template.registerHelper("timestampToTime", function (timestamp) {
+	var date = new Date(timestamp);
+	var hours = date.getHours();
+	var minutes = "0" + date.getMinutes();
+	var seconds = "0" + date.getSeconds();
+	return hours + ':' + minutes.substr(minutes.length-2) + ':' + seconds.substr(seconds.length-2);
+});
+
 Template.messages.helpers({
 	messages: Messages.find({})
 	// Can't use semi-colon here because it messages with messages being identified as a function
@@ -14,7 +33,11 @@ Template.footer.events({
 			var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
 			if (charCode == 13) {
 				e.stopPropagation();
-				Messages.insert({text: $('.input-box_text').val()});
+				Messages.insert({
+					text: $('.input-box_text').val(),
+					user: Meteor.userId(),
+					timestamp: Date.now()
+				});
 				$('.input-box_text').val("");
 				return false;
 			}
--- server/seeder.js
+++ server/seeder.js
@@ -2,6 +2,9 @@ Meteor.startup(function () {
 	Factory.define('message', Messages, {
 		text: function () {
 			return Fake.sentence();
+		},
+		timestamp: function() {
+			return Date.now();
 		}
 	});
 
