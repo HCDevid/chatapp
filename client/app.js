cc23f3c4dd35b36836a98c280eb967d7a0ea5138
Don Khan <HCDevid@gmail.com>
2016-01-19 21:03:23 -0500

    Added methods for new chat messages and userId && timestamp validation

--- client/app.js
+++ client/app.js
@@ -29,22 +29,4 @@ Template.messages.helpers({
 	// Can't use semi-colon here because it messages with messages being identified as a function
 })
 
-Template.footer.events({
-	'keypress input': function(e) {
-		var inputVal = $('.input-box_text').val();
-		if(!!inputVal) {
-			var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
-			if (charCode == 13) {
-				e.stopPropagation();
-				Messages.insert({
-					text: $('.input-box_text').val(),
-					user: Meteor.userId(),
-					timestamp: Date.now()
-				});
-				$('.input-box_text').val("");
-				return false;
-			}
-		}
-	}
-});
 
